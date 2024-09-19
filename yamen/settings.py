649538aac833a92ff34c29a91327132ce3df7d74
Yamen sh <yamen.2.sh@gmail.com>
2024-09-19 13:22:22 +0300

    for test vercel

--- yamen/settings.py
+++ yamen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0%kkl6dp8d(gq-d-(blm#n-re7k*!dd&x(i6pa17ej499^yor2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.com']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
